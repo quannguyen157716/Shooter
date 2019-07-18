ed61183799f7e65451bab8f7988d69b1e77fd753
Quan <quannguyen157716@gmail.com>
2019-07-18 07:35:17 +0700

    n

--- Assets/2DSpaceShooterExample/CompleteProject/SS_Scripts/PoolScripts/TestingScript.cs
+++ Assets/2DSpaceShooterExample/CompleteProject/SS_Scripts/PoolScripts/TestingScript.cs
@@ -3,44 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
+public static class JsonHelper
+{
+    public static T[] FromJson<T>(string json)
+    {
+        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+        return wrapper.Items;
+    }
+
+    public static string ToJson<T>(T[] array)
+    {
+        Wrapper<T> wrapper = new Wrapper<T>();
+        wrapper.Items = array;
+        return JsonUtility.ToJson(wrapper);
+    }
+
+    public static string ToJson<T>(T[] array, bool prettyPrint)
+    {
+        Wrapper<T> wrapper = new Wrapper<T>();
+        wrapper.Items = array;
+        return JsonUtility.ToJson(wrapper, prettyPrint);
+    }
+
+    [System.Serializable]
+    private class Wrapper<T>
+    {
+        public T[] Items;
+    }
+}
+
 public class AnOb
 {
 	public int a;
 }
 [System.Serializable]
 public class TestingS{
-	public int level; 
-	public int numberOfEnemy;	
+	public Vector3 position;
+	public int health;
+
 }
 
 public class TestingScript : MonoBehaviour {
 
-	TestingS myOb= new TestingS();
 	void Start () {
-		myOb.level=1;
-		myOb.numberOfEnemy=6;
-		string j=JsonUtility.ToJson(myOb,true);
-		Debug.Log(j);
+		/* myOb.position=new Vector3(1,1,3);
+		myOb.health=15;
+
+		string json=JsonUtility.ToJson(myOb,true);
+		Debug.Log(json);
 		Debug.Log(Application.persistentDataPath);
-		File.WriteAllText(Application.persistentDataPath+"/myOb.json", j);
-		string content=File.ReadAllText(Application.persistentDataPath+"/myOb.json");
-		myOb=JsonUtility.FromJson<TestingS>(content);
-		Debug.Log(Application.persistentDataPath);
-		//Debug.Log(content);
-		//Debug.Log(myOb.level);
-		Debug.Log(content.Length);
-		Debug.Log(myOb.numberOfEnemy);
-		/* File.WriteAllText(); 
+		File.WriteAllText(Application.persistentDataPath+"/SaveFile.json",json);*/
+		string json=File.ReadAllText(Application.persistentDataPath+"/SaveFile.json");
+		TestingS myOb=JsonUtility.FromJson<TestingS>(json);
+		Debug.Log(myOb.position);
+		Debug.Log(myOb.health); 
 //https://stackoverflow.com/questions/36239705/serialize-and-deserialize-json-and-json-array-in-unity
 //https://stackoverflow.com/questions/52141800/unity-serializing-object-to-json
 
 //
 // serialize JSON directly to a file
-		using (StreamWriter file = File.CreateText(@"c:\movie.json"))
-{
-    JsonSerializer serializer = new JsonSerializer();
-    	serializer.Serialize(file, movie);
-}*/
 	}
 	void WriteToJson()
 	{
